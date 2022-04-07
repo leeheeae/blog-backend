787fd5c0ea112c708267cbd23437ed8467565b84
leeheeae <chromeee@daum.net>
2022-04-07 17:31:02 +0900

    mongoose 연결

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
 
 //비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
-const { PORT } = process.env;
+const { PORT, MONGO_URI } = process.env;
+
+//mongoose
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 const api = require('./api');
 
